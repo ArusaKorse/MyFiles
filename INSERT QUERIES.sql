28adcec1d26236e8b7d7e5e4983d52e90e005d75
ARUSA KORSE <marousa07@gmail.com>
2021-10-25 12:44:05 +0530

    COMMIT FOR INSERT QUERIES

---
+++ INSERT QUERIES.sql
@@ -0,0 +1,9 @@
+DESC countries;
+
+INSERT INTO countries VALUES(1, 'india','111111');
+INSERT INTO countries VALUES(2, 'egypt','222222');
+INSERT INTO countries VALUES(3, 'africa','333333');
+INSERT INTO countries VALUES(4, 'uae','4444');
+
+SELECT * FROM countries;
+
